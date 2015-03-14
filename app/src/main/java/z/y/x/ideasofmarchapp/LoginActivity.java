1bad8623d7cca2ce24cae3fba90e7b5b962487d1
Daniel Pesqueda <dpesqueda@csumb.edu>
2015-03-13 21:06:37 -0700

    Changined MainActivity to LoginActivity Changed main_activity_view to login Added Login fields and button

--- app/src/main/java/z/y/x/ideasofmarchapp/LoginActivity.java
+++ app/src/main/java/z/y/x/ideasofmarchapp/LoginActivity.java
@@ -7,39 +7,24 @@ import android.view.MenuItem;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        String address = "http://10.11.173.205/iom/helloworld.php";
-        HttpClient client = new DefaultHttpClient();
-        HttpPost post = new HttpPost(address);
-
-        List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-        pairs.add(new BasicNameValuePair("username", "value1"));
-        pairs.add(new BasicNameValuePair("password", "value2"));
-        try{
-            post.setEntity(new UrlEncodedFormEntity(pairs));
-            HttpResponse response = client.execute(post);
-        }
-        catch(Exception e){
-
-        }
-
-
-
+        setContentView(R.layout.login);
     }
 
 
@@ -63,5 +48,27 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void postData() {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://www.yoursite.com/script.php");
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("userName", "pesq8691"));
+            nameValuePairs.add(new BasicNameValuePair("passWord", "passwordlol"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
     }
 }
